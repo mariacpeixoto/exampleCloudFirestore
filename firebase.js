2b77afc192e26fdc76c12e4ffaba400e0e058540
Maria Peixoto <83721653+mariacpeixoto@users.noreply.github.com>
2021-11-10 14:51:59 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyB8aE-BrjA2l1e3AzWrTrGU4uJWjus9PGk",
+    authDomain: "pwii-f6f07.firebaseapp.com",
+    projectId: "pwii-f6f07",
+    storageBucket: "pwii-f6f07.appspot.com",
+    messagingSenderId: "969640438653",
+    appId: "1:969640438653:web:a51fdca0eadd99fe2cba14",
+    measurementId: "G-MNF42TGY6N"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
